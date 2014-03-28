46253a1638d3ad273833a20c67e3a61f8d91c70e
cuihq <cuihaiqin@gmail.com>
2014-03-29 01:19:08 +0800

    change directory structure

--- app.rb
+++ app.rb
@@ -1,21 +1,32 @@
-%w(sinatra haml builder redcarpet rouge rouge/plugins/redcarpet).each { |v| require v }
+%w(sinatra haml builder sass redcarpet rouge rouge/plugins/redcarpet).each { |v| require v }
 class HTML < Redcarpet::Render::HTML
   include Rouge::Plugins::Redcarpet
 end
 set :markdown, layout_engine: :haml, renderer: HTML, fenced_code_blocks: true, disable_indented_code_blocks: true
+set :views, '.'
 set :environment, 'production'
 set :port, 80
 not_found { haml 'This is nowhere to be found.' }
 error { 'Sorry there was a error.' }
-
+$last_modified = Time.now
 get '/' do
-  articles = Dir['views/*.md'].map { |r| [File.basename(r, '.md'), File.mtime(r).strftime('%Y-%m-%d')] }
+  cache_control :public, :max_age => 72000
+  articles = Dir['*.md'].map { |r| [File.basename(r, '.md'), File.mtime(r).strftime('%Y-%m-%d')] }
   haml :index, locals: { archives: articles } 
+end
+
+get '/robots.txt' do
+  'Sitemap: http://cuihq.me/sitemap.xml'
+end
+
+get '/blog.css' do
+  cache_control :public, :max_age => 2592000
+  scss :stylesheet, :style => :expanded
 end
 
 get '/sitemap.xml' do
   content_type 'application/xml'
-  articles = Dir['views/*.md'].map { |r| [File.basename(r, '.md'), File.mtime(r).strftime('%Y-%m-%d')] }
+  articles = Dir['*.md'].map { |r| [File.basename(r, '.md'), File.mtime(r).strftime('%Y-%m-%d')] }
   builder :layout => false do |xml|
     xml.instruct! :xml, :version => '1.0'
     xml.urlset :xmlns =>"http://www.sitemaps.org/schemas/sitemap/0.9" do
@@ -32,13 +43,13 @@ end
 
 get '/rss' do
   content_type 'application/xml'
-  articles = Dir['views/*.md'].map { |r| [File.basename(r, '.md'), File.mtime(r).rfc822()] }
+  articles = Dir['*.md'].map { |r| [File.basename(r, '.md'), File.mtime(r).rfc822()] }
   builder :layout => false do |xml|
     xml.instruct! :xml, :version => '1.0'
     xml.rss :version => "2.0" do
       xml.channel do
-        xml.title "Bug's Blog RSS"
-        xml.description "Bug Blog Posts"
+        xml.title "cuihq's blog"
+        xml.description "cuihq's blog"
         xml.link "http://www.cuihq.me/"
         articles.each do |title, time|
           xml.item do
@@ -54,7 +65,7 @@ get '/rss' do
 end
 
 get '/article/:title' do |title|
-  if File.exist?("views/#{title}.md") then markdown title.to_sym else 404 end
+  if File.exist?("#{title}.md") then markdown title.to_sym else 404 end
 end
 
 __END__
@@ -70,55 +81,57 @@ __END__
 !!! 5
 %html
   %head
-    %title Cuihq's Blog
-    :css 
-      .header a, .footer a { color: #b83000; }
-      .container { margin: 0 auto; width: 80%; }
-      #rss { background-color: #b83000; color: white; padding: 3px; text-decoration: blink; font-size: small; }
-      .footer { color: #888; float: right;}
-      pre { font-family: 'Ubuntu Mono', 'Monaco', monospace;}
-      .highlight { color: #faf6e4; background-color: #122b3b; }
-      .highlight table td { padding: 5px; }
-      .highlight table pre { margin: 0; }
-      .highlight .gl, .highlight .gt { color: #dee5e7; background-color: #4e5d62; }
-      .highlight .c, .highlight .cd, .highlight .cm, .highlight .c1, .highlight .cs { color: #6c8b9f; font-style: italic; }
-      .highlight .cp { color: #b2fd6d; font-weight: bold; font-style: italic; }
-      .highlight .err, .highlight .gr { color: #fefeec; background-color: #cc0000; }
-      .highlight .k, .highlight .kd, .highlight .kv { color: #f6dd62; font-weight: bold; }
-      .highlight .o, .highlight .ow, .highlight .p, .highlight .pi { color: #4df4ff; }
-      .highlight .gd { color: #cc0000; }
-      .highlight .gi { color: #b2fd6d; }
-      .highlight .ge { font-style: italic; }
-      .highlight .gs { font-weight: bold; }
-      .highlight .kc { color: #f696db; font-weight: bold; }
-      .highlight .kn, .highlight .kp, .highlight .kr, .highlight .gh, .highlight .gu, .highlight .nl, .highlight .nt { color: #ffb000; font-weight: bold; }
-      .highlight .kt, .highlight .no, .highlight .nc, .highlight .nd, .highlight .nn, .highlight .bp, .highlight .ne { color: #b2fd6d; font-weight: bold; }
-      .highlight .m, .highlight .mf, .highlight .mh, .highlight .mi, .highlight .il, .highlight .mo, .highlight .mb, .highlight .mx, .highlight .ld,.highlight .ss { color: #f696db; font-weight: bold; }
-      .highlight .s, .highlight .sb, .highlight .sd, .highlight .s2, .highlight .sh, .highlight .sx, .highlight .sr, .highlight .s1 { color: #fff0a6; font-weight: bold; }
-      .highlight .se, .highlight .sc, .highlight .si { color: #4df4ff; font-weight: bold; }
-      .highlight .nb { font-weight: bold; }
-      .highlight .ni { color: #999999; font-weight: bold; }
-      .highlight .w { color: #BBBBBB; }
-      .highlight .nf, .highlight .py, .highlight .na { color: #a8e1fe; }
-      .highlight .nv, .highlight .vc, .highlight .vg, .highlight .vi { color: #a8e1fe; font-weight: bold; }
+    %title cuihq's blog
+    %link(rel='stylesheet' href='/blog.css' type='text/css')
   %body
     %h1.header
-      %a(href='/') Cuihq's Blog
+      %a(href='/') cuihq's blog
       %a#rss(href='/rss') RSS
     %hr/
     .container= yield
     #disqus_thread
-      %noscript Please enable JavaScript to view the comments powered by Disqus. 
+      %noscript Please enable JavaScript to view the comments. 
       :javascript
         var disqus_shortname = 'cuihqsblog';
-        (function() {
-          if (new RegExp('article').test(window.location.href)) {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-          }
-        })();
+        if (new RegExp('article').test(window.location.href)) {
+          var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+          dsq.src = '//cuihqsblog.disqus.com/embed.js';
+          (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+        }
     %hr/
     %h2.footer
       %i powered by
-      %a(href='https://github.com/cuihq/blog') Cuihq's Blog
+      %a(href='https://github.com/cuihq/blog') cuihq's blog
+
+@@ stylesheet
+.header a, .footer a { color: #b83000; }
+.container, #disqus_thread { margin: 0 auto; width: 80%; }
+#rss { background-color: #b83000; color: white; padding: 3px; text-decoration: blink; font-size: small; }
+.footer { color: #888; float: right; }
+pre { font-family: 'Ubuntu Mono', 'Monaco', monospace; }
+.highlight { 
+  color: #faf6e4; background-color: #122b3b;
+  table td { padding: 5px; }
+  table pre { margin: 0; }
+  .gl, .gt { color: #dee5e7; background-color: #4e5d62; }
+  .c, .cd, .cm, .c1, .cs { color: #6c8b9f; font-style: italic; }
+  .cp { color: #b2fd6d; font-weight: bold; font-style: italic; }
+  .err, .gr { color: #fefeec; background-color: #cc0000; }
+  .k, .kd, .kv { color: #f6dd62; font-weight: bold; }
+  .o, .ow, .p, .pi { color: #4df4ff; }
+  .gd { color: #cc0000; }
+  .gi { color: #b2fd6d; }
+  .ge { font-style: italic; }
+  .gs { font-weight: bold; }
+  .kc { color: #f696db; font-weight: bold; }
+  .kn, .kp, .kr, .gh, .gu, .nl, .nt { color: #ffb000; font-weight: bold; }
+  .kt, .no, .nc, .nd, .nn, .bp, .ne { color: #b2fd6d; font-weight: bold; }
+  .m, .mf,  .mh, .mi, .il, .mo, .mb, .mx, .ld, .ss { color: #f696db; font-weight: bold; }
+  .s, .sb, .sd, .s2, .sh, .sx, .sr, .s1 { color: #fff0a6; font-weight: bold; }
+  .se, .sc, .si { color: #4df4ff; font-weight: bold; }
+  .nb { font-weight: bold; }
+  .ni { color: #999999; font-weight: bold; }
+  .w { color: #BBBBBB; }
+  .nf, .py, .na { color: #a8e1fe; }
+  .nv, .vc, .vg, .vi { color: #a8e1fe; font-weight: bold; }
+}
