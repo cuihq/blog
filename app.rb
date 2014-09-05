478d3a38542f10cc15d414004efd56e4c6ec4587
superengine <cuihaiqin@gmail.com>
2014-09-05 13:24:31 +0800

    change 404 page

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ class HTML < Redcarpet::Render::HTML
 end
 set :markdown, layout_engine: :haml, renderer: HTML, fenced_code_blocks: true, disable_indented_code_blocks: true, tables: true, superscript: true
 set environment: 'production', port: 80, logging: nil, static_cache_control: [:public, :max_age => 300]
-not_found { "<iframe scrolling='no' frameborder='0' src='http://yibo.iyiyun.com/js/yibo404/key/1' width='640' height='462' style='display:block;'></iframe>" }
+not_found { "<iframe scrolling='no' frameborder='0' src='http://yibo.iyiyun.com/Home/Distribute/ad404/key/5953' width='654' height='470' style='display:block;'></iframe>" }
 err_logger = Logger.new('log/blog.log', 'monthly')
 error do
   err_logger.error env['sinatra.error']
